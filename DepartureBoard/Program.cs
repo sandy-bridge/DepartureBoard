44af2728e1143466446e4fcaeb8c446e75d3629c
Mihkel Kiil <kiil.mihkel@gmail.com>
2023-10-08 14:49:32 +0300

    extract method for getting stop departures

--- DepartureBoard/Program.cs
+++ DepartureBoard/Program.cs
@@ -3,6 +3,23 @@ using GTFS.Entities;
 using GTFS.IO;
 using static System.DateTime;
 
+List<string> Get5StopDepartureStrings(GTFSFeed gtfsFeed, int stopId)
+{
+    var departures = gtfsFeed.StopTimes.GetForStop(stopId.ToString())
+        .Where(t => t.DepartureTime > TimeOfDay.FromDateTime(Now))
+        .ToList();
+    departures.Sort((a, b) => (int) a.DepartureTime?.CompareTo(b.DepartureTime)!);
+    List<String> list = new List<string>();
+    foreach (var time in departures.Take(5))
+    {
+        var trip = gtfsFeed.Trips.FirstOrDefault(t => t.Id == time.TripId);
+        var route = gtfsFeed.Routes.FirstOrDefault(r => r.Id == trip?.RouteId);
+        list.Add(String.Format(time.DepartureTime + " " + route?.ShortName + " " + trip?.Headsign));
+    }
+
+    return list;
+}
+
 var httpClient = new HttpClient();
 Console.WriteLine("Downloading public transport data.");
 const string GTFS_URI = "https://peatus.ee/gtfs/gtfs.zip";
@@ -16,14 +33,8 @@ System.IO.Compression.ZipFile.ExtractToDirectory("gtfs.zip", "gtfs", true);
 var feed = reader.Read("gtfs");
 var test_stop_id = 908;
 Console.WriteLine("Stop ID " + test_stop_id + ": " + feed.Stops.FirstOrDefault(s=> int.Parse(s.Id) == test_stop_id)?.Name);
-var departures = feed.StopTimes.GetForStop(test_stop_id.ToString())
-    .Where(t => t.DepartureTime > TimeOfDay.FromDateTime(Now))
-    .ToList();
-departures.Sort((a, b) => (int) a.DepartureTime?.CompareTo(b.DepartureTime)!);
-
-foreach (var time in departures.Take(5))
+var departureStrings = Get5StopDepartureStrings(feed, test_stop_id);
+foreach (var departureString in departureStrings)
 {
-    var trip = feed.Trips.FirstOrDefault(t => t.Id == time.TripId);
-    var route = feed.Routes.FirstOrDefault(r => r.Id == trip?.RouteId);
-    Console.WriteLine(time.DepartureTime + " " + route?.ShortName + " "+ trip?.Headsign);
+    Console.WriteLine(departureString);
 }
