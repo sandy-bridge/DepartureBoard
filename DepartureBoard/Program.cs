a4761d364dae38fb090365fbd568bcca9e924f5a
Mihkel Kiil <kiil.mihkel@gmail.com>
2023-10-11 14:26:38 +0300

    fix download URI

--- DepartureBoard/Program.cs
+++ DepartureBoard/Program.cs
@@ -53,7 +53,7 @@ async Task<GTFSFeed> DownloadData(string gtfs_uri, bool overwrite = false)
     }
 }
 
-const String GTFS_URI = "https://peatus.ee/gtfs";
+const String GTFS_URI = "https://peatus.ee/gtfs/gtfs.zip";
 var feed = await DownloadData(GTFS_URI);
 var test_stop_id = 908;
 Console.WriteLine("Stop ID " + test_stop_id + ": " + feed.Stops.FirstOrDefault(s=> int.Parse(s.Id) == test_stop_id)?.Name);
