50ff46eb4d7cf969f3c7b3f038fc733d2d6e210b
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-01-05 18:27:22 +0200

    Fix typo

--- DepartureAPI/Program.cs
+++ DepartureAPI/Program.cs
@@ -1,9 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy =>
+    {
+        policy.WithOrigins("https://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -19,7 +28,7 @@ app.UseHttpsRedirection();
 const string GTFS_URI = "https://peatus.ee/gtfs/gtfs.zip";
 var feed = await GTFSActions.GTFSActions.DownloadData(GTFS_URI);
 
-app.MapGet("/stopdepatures/{stopId}", (string stopId) =>
+app.MapGet("/stopdepartures/{stopId}", (string stopId) =>
 {
     var stopName = feed.Stops.FirstOrDefault(s=> s.Id == stopId)?.Name ?? null;
     if(stopName == null){
@@ -28,5 +37,7 @@ app.MapGet("/stopdepatures/{stopId}", (string stopId) =>
     var list = GTFSActions.GTFSActions.Get5StopDepartureStrings(feed, int.Parse(stopId));
     return Results.Json(new { StopName = stopName, Departures = list});
 });
+
+app.UseCors();
 
 app.Run();
