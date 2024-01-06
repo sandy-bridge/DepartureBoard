a922ba6aeaf04e2095de8389df7781ba7732c7bc
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-01-06 19:09:43 +0200

    Put departures in list

--- departureweb/src/App.tsx
+++ departureweb/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { useStop } from './api/departure';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 function App() {
   const { isLoading, error, data } = useStop();
@@ -11,11 +13,13 @@ function App() {
   </div>
   );
   if (error) console.log('An error occurred while fetching the user data ', error);
-  const departures = data?.departures.map((departure: string) => <li>{departure}</li>)
+  const departures = data?.departures.map((departure: string) => <ListItem>{departure}</ListItem>)
   return (
     <div className="App">
       <h1>{data?.stopName}</h1>
-      <ul>{departures}</ul>
+      <List>
+        {departures}
+      </List>
     </div>
   );
 }
