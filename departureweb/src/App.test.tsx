85112d0c65d1678cd36c12f7889a0acda43166b5
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-01-21 15:08:01 +0200

    Fix test

--- departureweb/src/App.test.tsx
+++ departureweb/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('shows stop name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/A.H. Tammsaare tee/i);
   expect(linkElement).toBeInTheDocument();
 });
