0946bc3b96e8edfb8ff5fcb5376b9fbdde78f426
Mihkel Kiil <kiil.mihkel@gmail.com>
2024-12-30 23:42:00 +0200

    Multiple stops in one list

--- DepartureConsole/Program.cs
+++ DepartureConsole/Program.cs
@@ -2,7 +2,14 @@
 var feed = await GTFSActions.GTFSActions.DownloadData(GTFS_URI);
 var test_stop_id = 908;
 Console.WriteLine("Stop ID " + test_stop_id + ": " + feed.Stops.FirstOrDefault(s=> int.Parse(s.Id) == test_stop_id)?.Name);
-var departureStrings = GTFSActions.GTFSActions.Get5StopDepartureStrings(feed, test_stop_id);
+var departureStrings = GTFSActions.GTFSActions.GetStopDepartureStrings(feed, test_stop_id);
+foreach (var departureString in departureStrings)
+{
+    Console.WriteLine(departureString);
+}
+var test_stops = new List<int>{908, 872};
+departureStrings = GTFSActions.GTFSActions.GetMultipleStopsDepartureStrings(feed, test_stops);
+Console.WriteLine("Stop ID " + test_stops[0] + " and " + test_stops[1]);
 foreach (var departureString in departureStrings)
 {
     Console.WriteLine(departureString);
--- GTFSActions/GTFSActions.cs
+++ GTFSActions/GTFSActions.cs
@@ -3,6 +3,8 @@ using GTFS;
 using GTFS.Entities;
 using static System.DateTime;
 using System.IO.Compression;
+using System.Collections;
+
 public class GTFSActions
 {
     public static async Task<GTFSFeed> DownloadData(string gtfs_uri, bool overwrite = false)
@@ -41,14 +43,40 @@ public class GTFSActions
             return true;
         }
     }
-    public static List<string> Get5StopDepartureStrings(GTFSFeed gtfsFeed, int stopId)
+    public static List<string> GetMultipleStopsDepartureStrings (GTFSFeed gtfsFeed, List<int> stops, int n=5)
+    {
+        var departureLists = new List<List<StopTime>>();
+        stops.ForEach(s => departureLists.Add(gtfsFeed.StopTimes.GetForStop(s.ToString())
+            .Where(t => t.DepartureTime > TimeOfDay.FromDateTime(Now))
+            .ToList()));
+        departureLists.ForEach(d => d.Sort(
+            (a, b) => (int)a.DepartureTime?.CompareTo(b.DepartureTime)!));
+        var departureQueueList = new List<Queue<StopTime>>();
+        departureLists.ForEach(d => departureQueueList.Add(new Queue<StopTime>(d)));
+        var combinedList = new List<StopTime>();
+        while(departureQueueList.Any(s => s.Count > 0)){
+            combinedList.Add(departureQueueList.Aggregate((curMin, x) => (curMin == null || (x.Peek().DepartureTime) <
+    curMin.Peek().DepartureTime ? x : curMin)).Dequeue());
+        }
+        List<String> list = new List<string>();
+        foreach (var time in combinedList.Take(n))
+        {
+            var trip = gtfsFeed.Trips.FirstOrDefault(t => t.Id == time.TripId);
+            var route = gtfsFeed.Routes.FirstOrDefault(r => r.Id == trip?.RouteId);
+            var departureTime = time.DepartureTime?.ToString().Substring(0, 5);
+            list.Add(String.Format(departureTime + " " + route?.ShortName + " " + trip?.Headsign));
+        }
+
+        return list;
+    }
+    public static List<string> GetStopDepartureStrings(GTFSFeed gtfsFeed, int stopId, int n=5)
     {
         var departures = gtfsFeed.StopTimes.GetForStop(stopId.ToString())
             .Where(t => t.DepartureTime > TimeOfDay.FromDateTime(Now))
             .ToList();
         departures.Sort((a, b) => (int)a.DepartureTime?.CompareTo(b.DepartureTime)!);
         List<String> list = new List<string>();
-        foreach (var time in departures.Take(5))
+        foreach (var time in departures.Take(n))
         {
             var trip = gtfsFeed.Trips.FirstOrDefault(t => t.Id == time.TripId);
             var route = gtfsFeed.Routes.FirstOrDefault(r => r.Id == trip?.RouteId);
